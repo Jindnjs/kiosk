6d64c20fb1a219d5418ccce66439b037962eee86
Jindnjs <145753929+Jindnjs@users.noreply.github.com>
2025-04-24 21:00:18 +0900

    :memo: chore : add lv6

---
+++ src/lv6/Menu.java
@@ -0,0 +1,31 @@
+package lv6;
+
+import java.util.List;
+
+public class Menu {
+
+    private String categoryName;
+    //메뉴 아이템을 Menu클래스가 관리
+    private List<MenuItem> menuItems;
+
+    //생성자
+    public Menu(String categoryName, List<MenuItem> menuItems) {
+        this.categoryName = categoryName;
+        this.menuItems = menuItems;
+    }
+
+    //Getter
+    public String getCategoryName() {return categoryName;}
+    public List<MenuItem> getMenuItems() {return menuItems;}
+
+    //메뉴 아이템을 출력하는 메소드
+    public void displayMenu(){
+        // 카테고리이름의 대문자 출력
+        System.out.println("\n[ "+ categoryName.toUpperCase() +" MENU ]");
+
+        for (int i = 0; i < menuItems.size(); i++){
+            System.out.println(i+1 + ". " + menuItems.get(i));
+        }
+        System.out.println("0. 뒤로가기");
+    }
+}
