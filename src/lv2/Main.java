4af06f50db83ca7abb65ad11db949c009fac8f3d
Jindnjs <145753929+Jindnjs@users.noreply.github.com>
2025-04-24 13:28:38 +0900

    :recycle: refactor: 반복문 외부로 객체 생성을 이동

--- src/lv2/Main.java
+++ src/lv2/Main.java
@@ -7,19 +7,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        //메뉴 객체 저장할 컬렉션
+        List<MenuItem> menuItems = new ArrayList<MenuItem>();
+
+        //메뉴 객체 생성& 저장
+        menuItems.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+
         Scanner scanner = new Scanner(System.in);
 
         while (true){
-
-            //메뉴 객체 저장할 컬렉션
-            List<MenuItem> menuItems = new ArrayList<MenuItem>();
-
-            //메뉴 객체 생성& 저장
-            menuItems.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-            menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-            menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-            menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
-
             //메뉴 출력하기 -> 반복문&컬렉션 활용
             System.out.println("\n[ SHAKESHACK MENU ]");
 
